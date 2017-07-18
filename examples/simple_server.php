2a5f6c18fc008fb8e9160431ce172cb7d46eea0c
yym955 <261612423@qq.com>
2017-07-18 22:50:51 +0800

    Create simple_server.php

---
+++ examples/simple_server.php
@@ -0,0 +1,29 @@
+<?php
+////必须设置此目录,PHP程序的根目录
+ define("File_ROOT", __DIR__);
+
+require File_ROOT.'/swooleApi/autoload.php';
+
+//注册应用路径
+Huoniao\SwooleApi\Loader::addNameSpace('App', File_ROOT.'/App');
+spl_autoload_register('\\Huoniao\\SwooleApi\\Loader::autoload');
+
+
+ 
+//DI容器设置
+$di = \Huoniao\SwooleApi\Di::getInstance();
+$di->set("date",function(){
+	return time();
+}); 
+
+ 
+$serv = new Swoole\Http\Server("127.0.0.1", 9502);
+
+$serv->on('Request', function($request, $response){
+    
+    $rs = \Huoniao\SwooleApi\Dispatcher::execute($request,$response);
+    $response->end($rs); 
+});
+
+$serv->start();
+?>
